f2707dad6850d7bbe893ed1763cb74ecb87a17bd
강성문 <tjdans174@gmail.com>
2019-12-29 18:26:06 +0900

    feat : 유저 라우터 추가

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import UserController from '../controllers/user.controller';
-
+import routeValidate from '../middleware/routeValidate.middleware';
+import schemas from '../utils/schemas.util';
 export default class userRoute {
   path = '/user';
   router = Router();
@@ -11,8 +12,33 @@ export default class userRoute {
   }
 
   initializeRoutes() {
-    this.router.get(`${this.path}`, this.userController.getUsers);
-    this.router.post(`${this.path}`, this.userController.createUser);
-    this.router.post(`${this.path}/auth`, this.userController.loginUser);
+    this.router.get(
+      `${this.path}`,
+      this.validateGetUser,
+      this.userController.getUsers,
+    );
+    this.router.post(
+      `${this.path}`,
+      this.validatePostUser,
+      this.userController.createUser,
+    );
+    this.router.post(
+      `${this.path}/auth`,
+      this.validatePostUserAuth,
+      this.userController.loginUser,
+    );
+  }
+
+  validateGetUser(req, res, next) {
+    routeValidate(schemas.User.get, req);
+    next();
+  }
+  validatePostUser(req, res, next) {
+    routeValidate(schemas.User.post, req);
+    next();
+  }
+  validatePostUserAuth(req, res, next) {
+    routeValidate(schemas.User.auth.post, req);
+    next();
   }
 }
