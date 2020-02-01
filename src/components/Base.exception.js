908c7a2eb11d6ca7c3b2601f2cc78b764f66a48b
강성문 <tjdans174@gmail.com>
2020-02-01 20:24:34 +0900

    fix : 에러 처리 방법 변경

--- src/components/Base.exception.js
+++ src/components/Base.exception.js
@@ -1,12 +1,11 @@
-import { BaseException } from './Base.exception';
-
-export class ValidateException extends BaseException {
+export class BaseException extends Error {
   /**
    * @param message {string} Error 가 발생시 서버에서 클라이언트에 보낼 메세지
    * @param status {number} Error 가 발생시 서버에서 클라이언트에 보낼 상태코드
    */
-  constructor(message) {
-    super(message, 400);
-    this.message = `${message}`;
+  constructor({ message, status }) {
+    super(message);
+    this.status = status;
+    this.message = message;
   }
 }
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as ErrorMessage } from './ErrorMessage';
+export { BaseException as BaseError } from './Base.exception';
+export { default as Response } from './customResponse';
