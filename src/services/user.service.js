d3bd08c105d216a96010932716152444a5c1dac2
강성문 <tjdans174@gmail.com>
2020-02-13 20:27:08 +0900

    fix : service logic 에 lodash 를 이용하여 코드 축약

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,10 +1,12 @@
+import _ from 'lodash';
 import jsonwebtoken from 'jsonwebtoken';
 import { UserDao } from '../dao/user.dao';
-import { NotFoundResourceException } from '../exceptions/notFoundResource.exception';
+import { ErrorMessage, BaseError } from '../components';
+import Config from '../config';
 
 export class UserService {
   static createJWTToken(expiresIn, data) {
-    return jsonwebtoken.sign(data, process.env.PRIVATE_KEY, {
+    return jsonwebtoken.sign(data, Config.jwtSecret, {
       expiresIn,
       algorithm: 'HS256',
     });
@@ -16,7 +18,7 @@ export class UserService {
 
   static getUserListPaging(offset, limit) {
     return UserDao.findAndCountAll({
-      attributes: ['idx', 'name', 'email'],
+      attributes: ['idx', 'name', 'email', 'hash'],
       offset,
       limit,
     });
@@ -24,20 +26,16 @@ export class UserService {
 
   static async createUser(userDto) {
     const userRecord = await UserDao.create(userDto);
-    const { id, name, email } = userRecord.toJSON();
-    return {
-      id,
-      name,
-      email,
-    };
+    return _.pick(userRecord, ['id', 'name', 'email', 'hash']);
   }
 
   static async loginUser(email, password) {
     const userRecord = await UserDao.findOneOrFail({ where: { email } });
-    const userJson = userRecord.toJSON();
-    const isPassWord = UserDao.validPassword(password, userJson);
-    if (!isPassWord) throw new NotFoundResourceException('password');
-    const accessToken = await UserService.createAccessToken(userJson);
+    const isPassWord = userRecord.correctPassword(password);
+    if (!isPassWord) {
+      throw new BaseError(ErrorMessage.NotFoundResource('PassWord'));
+    }
+    const accessToken = await UserService.createAccessToken(userRecord);
     return { accessToken };
   }
 }
